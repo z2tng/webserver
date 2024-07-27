de80ff13417bad9ef06b3cff7612445f45114130
z2tng <tz_none@163.com>
2024-07-27 22:58:55 +0800

    feat(log): Implement LogStream class for C++ stream operations
    Implemented the LogStream class, which supports C++ stream-like operations. It converts various types of variables into char arrays for storage, facilitating easy logging and output formatting.
    
    实现了LogStream类，该类支持C++流式操作。它将各种类型的变量转换为char数组存储，便于日志记录和输出格式化。

---
+++ webserver/include/log/log_stream.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "utils/uncopyable.h"
+
+#include <strings.h>
+#include <string.h>
+#include <string>
+
+namespace log {
+
+const int kSmallBufferSize = 4 * 1024;
+const int kLargeBufferSize = 4 * 1024 * 1024;
+
+template <int buffer_size>
+class FixedBuffer : utils::Uncopyable {
+public:
+    FixedBuffer() : size_(0) {
+        bzero(buffer_, sizeof(buffer_));
+        current_ = buffer_;
+    }
+
+    ~FixedBuffer() {}
+
+    void write(const char* buffer, int size) {
+        if (capacity() > size) {
+            memcpy(current_, buffer, size);
+            current_ += size;
+            size_ += size;
+        }
+    }
+
+    void add(int size) {
+        current_ += size;
+        size_ += size;
+    }
+
+    void reset() {
+        current_ = buffer_;
+        size_ = 0;
+    }
+
+    const char* buffer() const { return buffer_; }
+    char* current() { return current_; }
+    int size() const { return size_; }
+    int capacity() const { return buffer_size - size_; }
+
+private:
+    char buffer_[buffer_size];
+    char* current_;
+    int size_;
+};
+
+class LogStream : utils::Uncopyable {
+public:
+    using Buffer = FixedBuffer<kSmallBufferSize>;
+
+    LogStream() {}
+    ~LogStream() {}
+
+    LogStream& operator<<(bool);
+
+    LogStream& operator<<(short);
+    LogStream& operator<<(unsigned short);
+    LogStream& operator<<(int);
+    LogStream& operator<<(unsigned int);
+    LogStream& operator<<(long);
+    LogStream& operator<<(unsigned long);
+    LogStream& operator<<(long long);
+    LogStream& operator<<(unsigned long long);
+
+    LogStream& operator<<(float);
+    LogStream& operator<<(double);
+    LogStream& operator<<(long double);
+
+    LogStream& operator<<(char);
+    LogStream& operator<<(const char*);
+    LogStream& operator<<(const unsigned char*);
+    LogStream& operator<<(const std::string&);
+
+    void Write(const char* buffer, int size) { buffer_.write(buffer, size); }
+    const Buffer& buffer() const { return buffer_; }
+    void reset() { buffer_.reset(); }
+
+private:
+    template <typename T>
+    void FormatInteger(T value);
+
+    static const int kMaxNumberSize = 32;
+
+    Buffer buffer_;
+};
+    
+} // namespace log
---
+++ webserver/src/log/log_stream.cc
@@ -0,0 +1,124 @@
+#include "log/log_stream.h"
+
+namespace log {
+
+template <typename T>
+size_t IntToString(char *buffer, T value) {
+    char *buf = buffer;
+    bool is_negative = false;
+
+    if (value < 0) {
+        is_negative = true;
+        value = -value;
+    }
+
+    while (value) {
+        *buf++ = '0' + value % 10;
+        value /= 10;
+    }
+
+    if (is_negative) *buf++ = '-';
+    *buf = '\0';
+
+    std::reverse(buffer, buf);
+    return buf - buffer;
+}
+
+template <typename T>
+void LogStream::FormatInteger(T value) {
+    if (buffer_.capacity() >= kMaxNumberSize) {
+        char buf[kMaxNumberSize];
+        int len = IntToString(buf, value);
+        Write(buf, len);
+    }
+}
+
+LogStream& LogStream::operator<<(bool value) {
+    Write(value ? "1" : "0", 1);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(short value) {
+    return *this << static_cast<int>(value);
+}
+
+LogStream& LogStream::operator<<(unsigned short value) {
+    return *this << static_cast<unsigned int>(value);
+}
+
+LogStream& LogStream::operator<<(int value) {
+    FormatInteger(value);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned int value) {
+    FormatInteger(value);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(long value) {
+    FormatInteger(value);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned long value) {
+    FormatInteger(value);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(long long value) {
+    FormatInteger(value);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned long long value) {
+    FormatInteger(value);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(float value) {
+    return *this << static_cast<double>(value);
+}
+
+LogStream& LogStream::operator<<(double value) {
+    if (buffer_.capacity() >= kMaxNumberSize) {
+        char buf[kMaxNumberSize];
+        int len = snprintf(buf, kMaxNumberSize, "%.12g", value);
+        Write(buf, len);
+    }
+    return *this;
+}
+
+LogStream& LogStream::operator<<(long double value) {
+    if (buffer_.capacity() >= kMaxNumberSize) {
+        char buf[kMaxNumberSize];
+        int len = snprintf(buf, kMaxNumberSize, "%.12Lg", value);
+        Write(buf, len);
+    }
+    return *this;
+}
+
+LogStream& LogStream::operator<<(char value) {
+    Write(&value, 1);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(const char* value) {
+    if (value) {
+        Write(value, strlen(value));
+    } else {
+        Write("(null)", 6);
+    }
+    return *this;
+}
+
+LogStream& LogStream::operator<<(const unsigned char* value) {
+    return operator<<(reinterpret_cast<const char*>(value));
+}
+
+LogStream& LogStream::operator<<(const std::string& value) {
+    Write(value.c_str(), value.size());
+    return *this;
+}
+
+}
